7eb5cc2d2ffa82a50661b182531d77fb6f39a6b1
Christian Schulze <christian.schulze@mni.thm.de>
2014-08-05 09:46:10 +0200

    grunt deployment workflow added

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,6 +6,51 @@ module.exports = function(grunt) {
             css: './public/css',
             js: ['*.js', 'public/js/*.js', 'test/*.js']
         },
+        buildType: 'Build',
+        pkg: grunt.file.readJSON('package.json'),
+        archive_name: grunt.option('name') || 'badeseen',
+
+        clean: {
+            pre: ['dist/', 'build/'],
+            post: ['<%= archive_name %>.zip']
+        },
+
+        compress: {
+            main: {
+                options: {
+                    archive: '<%= archive_name %>.zip'
+                },
+                expand: true,
+                cwd: 'build/',
+                src: ['**/*'],
+                dest: ''
+            }
+        },
+
+        copy: {
+            main: {
+                files: [{
+                    expand: true,
+                    src: ['public/css/**'],
+                    dest: 'build/'
+                }, {
+                    expand: true,
+                    src: ['public/js/**'],
+                    dest: 'build/'
+                }, {
+                    expand: true,
+                    src: ['index.html'],
+                    dest: 'build/'
+                }]
+            },
+            archive: {
+                files: [{
+                    expand: true,
+                    src: ['<%= archive_name %>.zip'],
+                    dest: 'dist/'
+                }]
+            }
+        },
 
         jshint: {
             src: '<%= paths.js %>'
@@ -43,6 +88,9 @@ module.exports = function(grunt) {
         }
     });
 
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-compress');
+    grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-sass');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-jshint');
@@ -50,4 +98,5 @@ module.exports = function(grunt) {
 
     grunt.registerTask('default', ['jshint', 'jsbeautifier:check', 'sass:admin']);
     grunt.registerTask('beautify', ['jsbeautifier:beautify']);
+    grunt.registerTask('deploy', ['jsbeautifier:beautify', 'clean:pre', 'copy:main', 'compress', 'copy:archive', 'clean:post']);
 };

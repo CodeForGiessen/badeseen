c4979a90fcb3298b4e00577a1bb0905389899575
Christian Schulze <christian.schulze@mni.thm.de>
2014-08-05 09:10:01 +0200

    added sass compile, jsbeautifier, jshint and watch

---
+++ Gruntfile.js
@@ -0,0 +1,53 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        paths: {
+            scss: './sass',
+            css: './public/css',
+            js: ['*.js', 'public/js/**/*.js', 'test/*.js']
+        },
+
+        jshint: {
+            src: '<%= paths.js %>'
+        },
+
+        jsbeautifier: {
+            beautify: {
+                src: '<%= paths.js %>'
+            },
+            check: {
+                src: '<%= paths.js %>',
+                options: {
+                    mode: 'VERIFY_ONLY'
+                }
+            }
+        },
+
+        sass: {
+            admin: {
+                files: {
+                    '<%= paths.css %>/main.css': '<%= paths.scss %>/main.scss',
+                }
+            }
+        },
+
+        watch: {
+            sass: {
+                files: './sass/**/*.scss',
+                tasks: ['sass:admin']
+            },
+            js: {
+                files: '<%= paths.js %>',
+                tasks: ['jshint']
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-jsbeautifier');
+
+    grunt.registerTask('default', ['jshint', 'jsbeautifier:check', 'sass:admin']);
+    grunt.registerTask('beautify', ['jsbeautifier:beautify']);
+};

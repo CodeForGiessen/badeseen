eafb3e1bb4ce66719e2f602c9a701032297de40b
Christian Schulze <christian.schulze@mni.thm.de>
2014-08-09 15:11:41 +0200

    moved sea list generation into lib.js

--- public/js/main.js
+++ public/js/main.js
@@ -2,27 +2,6 @@ var myApp = angular.module('myApp', ['ui.bootstrap']);
 
 
 
-/**
- * Return list of seas
- *
- * @discussion Each sea object has a unique name and a brief description
- * to display.
- * 
- * @return {Array of Objects} Array of sea objects
- */
-function getListOfSeasWithDescription() {
-	return [
-		{
-			'name': 'John Maynard',
-			'description': 'John Maynard! „Wer ist John Maynard?“ „John Maynard war unser Steuermann, aushielt er, bis er das Ufer gewann, er hat uns gerettet, er trägt die Kron’, er starb für uns, unsre Liebe sein Lohn. John Maynard.“'
-		},
-		{
-			'name': 'Mackie Messer',
-			'description': 'Wo ist Alfons gleich, der Fuhrherr? Kommt das je ans Sonnenlicht? Wer es immer wissen könnte Mackie Messer weiss es nicht.'
-		}
-	];
-}
-
 myApp.controller('SeaListCtrl', function($scope, $timeout, $modal, $log) {
 	$scope.seaList = getListOfSeasWithDescription();
 });

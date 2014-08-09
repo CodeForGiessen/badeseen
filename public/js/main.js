61d154f9409cac5937644ca19d13d9bd2b98db63
Christian Schulze <christian.schulze@mni.thm.de>
2014-08-09 15:34:12 +0200

    implemented user location retrievement that returns promise

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,32 @@
 var myApp = angular.module('myApp', ['ui.bootstrap']);
 
 
+myApp.factory('UserLocationService', function($q) {
+	var location;
+
+	function getUserLocation() {
+		var defer = $q.defer();
+
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(
+				function success(pos) {
+				defer.resolve({
+					'lat' : pos.coords.latitude,
+					'lng' : pos.coords.longitude
+				});
+			}, function error() {
+				defer.reject();
+			});
+		}
+
+		return defer.promise;
+	}
+
+	// return service api
+	return {
+		getUserLocation: getUserLocation
+	}
+});
 
 myApp.controller('SeaListCtrl', function($scope, $timeout, $modal, $log) {
 	$scope.seaList = getListOfSeasWithDescription();
